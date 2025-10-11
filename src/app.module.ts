f3a3a0140f41dd099c96def41574c3ee579091c7
mostafa <14712022100624@stud.cu.edu.eg>
2025-10-11 12:52:33 +0300

    chore: setup the config module to access env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { RedisModule } from './redis/redis.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule, RedisModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule, RedisModule],
   controllers: [],
   providers: [],
 })
