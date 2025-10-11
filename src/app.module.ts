2b349243944893cdb441901cecade138f91246a0
mostafa <14712022100624@stud.cu.edu.eg>
2025-10-11 13:04:28 +0300

    chore: setup the prisma module to be used globally with service injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { RedisModule } from './redis/redis.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule, RedisModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UsersModule,
+    RedisModule,
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor(config: ConfigService) {
+    super({
+      datasourceUrl: config.get('DATABASE_URL'),
+    });
+  }
+}
