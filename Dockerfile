3108d520dad72526c357ba2dca6aafa72997c282
Loay Ahmed <loayahmed304@gmail.com>
2025-10-12 19:16:43 +0300

    chore: setup code for openapi diff workflow and merge old config commits - [CU-869atb9ex] (#4)
    Co-authored-by: mostafa <14712022100624@stud.cu.edu.eg>

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM node:lts-alpine AS builder
 RUN npm install -g pnpm
 WORKDIR /app
 COPY package.json pnpm-lock.yaml ./
-RUN pnpm install --frozen-lockfile 
+RUN pnpm install --frozen-lockfile --ignore-scripts
 
 COPY prisma ./prisma
 RUN pnpm prisma generate
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { RedisModule } from './redis/redis.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, RedisModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UsersModule,
+    RedisModule,
+    PrismaModule,
+  ],
   controllers: [],
   providers: [],
 })
