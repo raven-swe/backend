f2328c92a5698e4a15034d76584f4122b1cf9588
mostafa <14712022100624@stud.cu.edu.eg>
2025-10-15 16:44:28 +0300

    refactor: add a decorator to get the user object from the request

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -528,8 +528,10 @@ export class AuthService {
     }
     return { exists: false };
   }
-  async login(user: Express.User) {
-    //login logic
-    return Promise.resolve(user);
+  async login(user: RequestUser) {
+    const payload = { username: user.identifier, sub: user.id };
+    return Promise.resolve({
+      access_token: this.jwtService.sign(payload),
+    });
   }
 }
