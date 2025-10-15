a9c92a2871c1234d433ce61075c673efa58913a6
mostafa <14712022100624@stud.cu.edu.eg>
2025-10-15 16:59:26 +0300

    feat: add the passport and jwt modules to the auth module imports and configure jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,6 +8,9 @@ import { BullModule } from '@nestjs/bullmq';
 import { RefreshTokensModule } from 'src/refresh-tokens/refresh-tokens.module';
 import { DevicesModule } from 'src/device/device.module';
 import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -22,6 +25,23 @@ import { LocalStrategy } from './local.strategy';
     JwtModule,
     BullModule.registerQueue({
       name: 'email',
+    PassportModule,
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const secret = configService.get<string>('JWT_SECRET');
+        if (!secret) {
+          throw new Error('JWT_SECRET environment variable not set!');
+        }
+        return {
+          secret: secret,
+          signOptions: {
+            expiresIn: configService.get<number>('JWT_EXPIRES_IN_SECONDS') || 15 * 60, // 15 mins
+          },
+        };
+      },
     }),
   ],
   controllers: [AuthController],
