f3cc98a922cd558753f20300b4b0cd6196532f2a
mostafa <14712022100624@stud.cu.edu.eg>
2025-10-15 17:22:31 +0300

    feat: add the jwt auth guard this should be used over all the endpoint that need the user to be authenticated

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,6 +11,7 @@ import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -45,6 +46,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
