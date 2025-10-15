9fb65ca3312432e308b0454ea582032a1a660e4d
mostafa <14712022100624@stud.cu.edu.eg>
2025-10-15 17:21:11 +0300

    feat: add the jwt strategy and make sure the jwt payload and the request user are the same

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -583,7 +583,7 @@ export class AuthService {
     return { refreshToken: newRefreshToken, accessToken };
   }
   async login(user: RequestUser) {
-    const payload = { username: user.identifier, sub: user.id };
+    const payload: RequestUser = user;
     return Promise.resolve({
       access_token: this.jwtService.sign(payload),
     });
