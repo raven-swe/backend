38c5b3c443a69e3b592643e84dea7116bf2bb126
omargamal10 <ogamal1011@gmail.com>
2025-10-16 18:52:45 +0300

    feat: add interfaces for unified api responses

---
+++ src/common/interfaces/response.interface.ts
@@ -0,0 +1,30 @@
+export interface ApiResponseBase {
+  success: boolean;
+  message?: string;
+}
+
+export interface ApiErrorResponse extends ApiResponseBase {
+  success: false;
+  error: {
+    code: string;
+    message: string;
+  };
+}
+
+export interface ApiValidationErrorResponse extends ApiResponseBase {
+  success: false;
+  error: {
+    code: 'VALIDATION_ERROR';
+    message: string;
+    errors: {
+      field: string;
+      code: string;
+      message?: string;
+    }[];
+  };
+}
+
+export interface ApiSuccessResponse<T> extends ApiResponseBase {
+  success: true;
+  data: T;
+}
