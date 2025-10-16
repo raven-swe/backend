4295e905c437af55b5c19c08d579f259b219da5b
mostafa <14712022100624@stud.cu.edu.eg>
2025-10-17 02:27:56 +0300

    feat: add validation for the login before starting the passport local strategy

--- src/auth/local-auth.guard.ts
+++ src/auth/local-auth.guard.ts
@@ -1,5 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ExecutionContext, BadRequestException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { validateOrReject, ValidationError } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
+import { Request } from 'express';
+import { LoginDto } from './dtos';
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+    const body: unknown = request.body;
+
+    const dto = plainToInstance(LoginDto, body);
+
+    try {
+      await validateOrReject(dto);
+    } catch (errors) {
+      if (Array.isArray(errors) && errors.every((e) => e instanceof ValidationError)) {
+        const messages = errors
+          .map((err: ValidationError) => (err.constraints ? Object.values(err.constraints) : []))
+          .flat();
+
+        throw new BadRequestException(messages);
+      }
+      throw errors;
+    }
+
+    return (await super.canActivate(context)) as boolean;
+  }
+}
