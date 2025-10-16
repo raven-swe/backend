aad85f5dd95c4c1fdb23a71fcf45215816537210
omargamal10 <ogamal1011@gmail.com>
2025-10-16 19:07:43 +0300

    chore: configure bullmq to use the existing redis instance

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { RedisModule } from './redis/redis.module';
@@ -10,6 +11,12 @@ import { RecaptchaModule } from './recaptcha/recaptcha.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    BullModule.forRoot({
+      connection: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
+      },
+    }),
     AuthModule,
     UsersModule,
     RedisModule,
