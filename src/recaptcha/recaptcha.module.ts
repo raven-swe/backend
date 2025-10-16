f7d8699597249810947d2088fa52f77072d1c144
omargamal10 <ogamal1011@gmail.com>
2025-10-16 03:49:24 +0300

    feat: add recaptcha guard for convenience

--- src/recaptcha/recaptcha.module.ts
+++ src/recaptcha/recaptcha.module.ts
@@ -1,8 +1,10 @@
 import { Logger, Module } from '@nestjs/common';
 import { RecaptchaService } from './recaptcha.service';
 import { HttpService } from '@nestjs/axios';
+import { RecaptchaGuard } from './recaptcha.guard';
 
 @Module({
   providers: [RecaptchaService, Logger, HttpService],
+  exports: [RecaptchaGuard],
 })
 export class RecaptchaModule {}
