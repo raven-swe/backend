6a7c13a9c0767901d8f29de8a680fe3747b33980
omargamal10 <ogamal1011@gmail.com>
2025-10-16 19:12:48 +0300

    feat: add basic functions for setting, retreival and deletion of redis keys

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -29,4 +29,20 @@ export class RedisService {
   getClient(): Redis {
     return this.redis;
   }
+
+  async set(key: string, value: string, ttlSeconds?: number): Promise<void> {
+    if (ttlSeconds) {
+      await this.redis.set(key, value, 'EX', ttlSeconds);
+    } else {
+      await this.redis.set(key, value);
+    }
+  }
+
+  async get(key: string): Promise<string | null> {
+    return this.redis.get(key);
+  }
+
+  async del(key: string): Promise<number> {
+    return this.redis.del(key);
+  }
 }
