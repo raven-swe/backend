e674759daea86514d9dc20a5c35b5c8e0d437662
omargamal10 <ogamal1011@gmail.com>
2025-10-16 20:24:30 +0300

    feat: add register endpoints and service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { RecaptchaModule } from 'src/recaptcha/recaptcha.module';
+import { JwtModule } from '@nestjs/jwt';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    RecaptchaModule,
+    JwtModule,
+    BullModule.registerQueue({
+      name: 'email',
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, Logger],
 })
 export class AuthModule {}
