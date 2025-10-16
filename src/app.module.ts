e1a7c8bcf66c63a09c78442ccc8f1a033e080740
omargamal10 <ogamal1011@gmail.com>
2025-10-16 22:40:46 +0300

    feat: add ip ratelimiter and db ports to env example

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,6 @@ import { UsersModule } from './users/users.module';
 import { RedisModule } from './redis/redis.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
-import { EmailModule } from './email/email.module';
-import { RecaptchaModule } from './recaptcha/recaptcha.module';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 
 @Module({
