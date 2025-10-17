0e98302ab210fa0a4395479e4814c66f62d21783
omargamal10 <ogamal1011@gmail.com>
2025-10-17 06:00:33 +0300

    feat: add Logger as a dependency for tests

--- src/email/email.service.spec.ts
+++ src/email/email.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EmailService } from './email.service';
+import { ConfigModule } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 describe('EmailService', () => {
   let service: EmailService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EmailService],
+      imports:[ConfigModule.forRoot()],
+      providers: [EmailService, Logger],
     }).compile();
 
     service = module.get<EmailService>(EmailService);
--- src/recaptcha/recaptcha.service.spec.ts
+++ src/recaptcha/recaptcha.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecaptchaService } from './recaptcha.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 describe('RecaptchaService', () => {
   let service: RecaptchaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RecaptchaService],
+      imports:[HttpModule, ConfigModule.forRoot()],
+      providers: [RecaptchaService, Logger],
     }).compile();
 
     service = module.get<RecaptchaService>(RecaptchaService);
