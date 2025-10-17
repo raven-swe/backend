f1db36870074edadd0f121a982fb37f8701f4ed0
omargamal10 <ogamal1011@gmail.com>
2025-10-17 05:12:33 +0300

    feat: add stubs for refresh token module, I won't both with 'devices' for now so this will be populated later

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { HttpExceptionFilter } from './common/filters/http-response.filter';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { RefreshTokensModule } from './refresh-tokens/refresh-tokens.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
     PrismaModule,
     EmailModule,
     RecaptchaModule,
+    RefreshTokensModule,
   ],
   controllers: [],
   providers: [
