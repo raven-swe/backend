14c9c75f8f40cad34a99c21fee6557b53f91785a
omargamal10 <ogamal1011@gmail.com>
2025-10-17 05:08:52 +0300

    feat: add endpoints for resending otp, checking email

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { StartRegistrationDto } from './dto/start-registration.dto';
 import { VerifyOtpDto } from './dto/verify-otp.dto';
 import { CompleteRegistrationDto } from './dto/complete-registration.dto';
+import { CheckEmailDto } from './dto/CheckEmailDto';
+import { ResendOtpDto } from './dto/resend-otp.dto';
 import { RecaptchaFailedException } from './exceptions/recaptcha.exception';
+import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -24,7 +27,33 @@ export class AuthController {
   }
 
   @Post('register/complete')
-  async completeRegistration(@Body() completeRegistrationDto: CompleteRegistrationDto) {
-    return await this.authService.completeRegistration(completeRegistrationDto);
+  async completeRegistration(
+    @Body() completeRegistrationDto: CompleteRegistrationDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { accessToken, refreshToken } =
+      await this.authService.completeRegistration(completeRegistrationDto);
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  @Post('register/resend-otp')
+  async resendOtp(@Body() creationTokenDto: ResendOtpDto) {
+    return await this.authService.resendOtp(creationTokenDto.creationToken);
+  }
+
+  @Get('check-email')
+  async checkEmail(@Query() checkEmailDto: CheckEmailDto) {
+    return await this.authService.checkEmail(checkEmailDto.email);
   }
 }
