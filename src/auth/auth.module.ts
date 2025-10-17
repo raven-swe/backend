0cccb3f2f5cc0f52f40b183b1a1d9e76217da858
omargamal10 <ogamal1011@gmail.com>
2025-10-17 05:11:36 +0300

    feat: add email rate-limiting for resend otp, persist a user to the database (uses email as username for now)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,18 +4,22 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { RecaptchaModule } from 'src/recaptcha/recaptcha.module';
 import { JwtModule } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
 import { BullModule } from '@nestjs/bullmq';
-
 @Module({
   imports: [
     UsersModule,
     RecaptchaModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'default_secret_key',
+      signOptions: { expiresIn: '30m' },
+    }),
     JwtModule,
     BullModule.registerQueue({
       name: 'email',
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, Logger],
+  providers: [AuthService, Logger, UsersService],
 })
 export class AuthModule {}
