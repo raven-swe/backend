f4991e67bd902fdba54ddbd8f0f434a107663c57
omargamal10 <ogamal1011@gmail.com>
2025-10-17 11:44:31 +0300

    chore: run formatter

--- src/refresh-tokens/refresh-tokens.module.ts
+++ src/refresh-tokens/refresh-tokens.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { RefreshTokensService } from './refresh-tokens.service';
 
 @Module({
-  providers: [RefreshTokensService]
+  providers: [RefreshTokensService],
 })
 export class RefreshTokensModule {}
