4e70fa07b2a34f4dfea62d090e09a530b82fc4b7
omargamal10 <ogamal1011@gmail.com>
2025-10-17 12:52:08 +0300

    chore: provide dependncies to tests so they run and remove tests for prettierignore (will add tests later)

--- src/prisma/prisma.service.spec.ts
+++ src/prisma/prisma.service.spec.ts
@@ -3,17 +3,21 @@ import { PrismaService } from './prisma.service';
 import { ConfigModule } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 
-
 describe('PrismaService', () => {
   let service: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports:[ConfigModule.forRoot()],
-      providers: [PrismaService, Logger],
+      imports: [ConfigModule.forRoot()],
+      providers: [PrismaService],
     }).compile();
 
     service = module.get<PrismaService>(PrismaService);
+    await service.$connect();
+  });
+
+  afterAll(async () => {
+    await service.$disconnect();
   });
 
   it('should be defined', () => {
--- src/recaptcha/recaptcha.service.spec.ts
+++ src/recaptcha/recaptcha.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecaptchaService } from './recaptcha.service';
+import { Logger } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 describe('RecaptchaService', () => {
   let service: RecaptchaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RecaptchaService],
+      imports: [ConfigModule.forRoot(), HttpModule],
+      providers: [RecaptchaService, Logger],
     }).compile();
 
     service = module.get<RecaptchaService>(RecaptchaService);
