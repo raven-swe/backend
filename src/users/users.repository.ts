d5e7923c60b671eb5886c05c7538bfbdf83eca0b
omargamal10 <ogamal1011@gmail.com>
2025-10-17 05:09:54 +0300

    feat: add user repository

---
+++ src/users/users.repository.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { LanguageCode } from '@prisma/client';
+
+@Injectable()
+export class UsersRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findByEmail(email: string) {
+    return await this.prisma.users.findUnique({ where: { email } });
+  }
+
+  async createUser({
+    email,
+    password,
+    birthDate,
+    languageCode,
+  }: {
+    email: string;
+    username: string;
+    password: string;
+    birthDate: Date;
+    languageCode: LanguageCode;
+  }) {
+    return await this.prisma.users.create({
+      data: {
+        email,
+        username: email,
+        password_hash: password,
+        language_code: languageCode,
+        birthdate: birthDate,
+      },
+    });
+  }
+}
