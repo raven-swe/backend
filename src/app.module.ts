01b560dc1758c4078d474dfe85083cd987c0c4a4
Omar Gamal <ogamal1011@gmail.com>
2025-10-18 16:05:56 +0300

    feat: ratelimting-ip - [CU-869avp74y] (#17)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,34 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { RedisModule } from './redis/redis.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60_000,
+          limit: 60, // 60 requests per minute
+        },
+      ],
+    }),
     AuthModule,
     UsersModule,
     RedisModule,
     PrismaModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
