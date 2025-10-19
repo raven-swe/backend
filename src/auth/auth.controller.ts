83df1070157e2933e98b802e1283c6876ba86323
omargamal10 <ogamal1011@gmail.com>
2025-10-19 21:18:03 +0300

    chore: remove test endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,4 @@
 import { Controller, Get } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {
-  @Get('status')
-  getStatus() {
-    return { status: 'Auth service is running' };
-  }
-}
+export class AuthController {}
