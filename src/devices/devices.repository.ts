d4f235ca73d283bd69ccadb2b7033a45b84ece89
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-10-20 02:59:10 +0300

    refactor(devices): remove unused import
    Signed-off-by: Tasneemmhammed0 <tasneemmohammed098@gmail.com>

--- src/devices/devices.repository.ts
+++ src/devices/devices.repository.ts
@@ -1,10 +1,8 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class DevicesRepository {
-  private readonly logger = new Logger(DevicesRepository.name);
-
   constructor(private readonly prisma: PrismaService) {}
 
   async removeAllUserDevices(userId: bigint) {
