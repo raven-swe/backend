362c4853df3be86e897ddbc529e118ccef86f1f6
omargamal10 <ogamal1011@gmail.com>
2025-10-20 05:00:42 +0300

    fix: lint

--- src/prisma/prisma.service.spec.ts
+++ src/prisma/prisma.service.spec.ts
@@ -1,7 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from './prisma.service';
 import { ConfigModule } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
 
 describe('PrismaService', () => {
   let service: PrismaService;
