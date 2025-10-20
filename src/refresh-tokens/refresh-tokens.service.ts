4da147b3865a5e8df8ea48bacde28d2b1c71c67f
omargamal10 <ogamal1011@gmail.com>
2025-10-20 04:15:29 +0300

    feat: add service and repository for users and refresh tokens

--- src/refresh-tokens/refresh-tokens.service.ts
+++ src/refresh-tokens/refresh-tokens.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { RefreshTokensRepository } from './refresh-tokens.repository';
+import { RefreshToken } from './interfaces/refresh-token.interface';
+import { Logger } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class RefreshTokensService {}
+export class RefreshTokensService {
+  private readonly logger = new Logger(RefreshTokensService.name);
+
+  constructor(
+    private readonly refreshTokensRepository: RefreshTokensRepository,
+    private readonly prisma: PrismaService,
+  ) {}
+
+  // service methods that take tx as a parameter for transactions are ones that will be used in a transaction by *another service*
+  async createRefreshToken(refreshToken: RefreshToken, tx: Prisma.TransactionClient = this.prisma) {
+    const token = await this.refreshTokensRepository.createRefreshToken(refreshToken, tx);
+    this.logger.log('Refresh token created successfully for user ID: ' + refreshToken.userId);
+    return token;
+  }
+}
