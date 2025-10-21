10ab9c1f2b511b82f925d0154ee6c4c680a0ef0e
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-10-22 01:10:25 +0300

    test(util): password utils unit tests
    Signed-off-by: Tasneemmhammed0 <tasneemmohammed098@gmail.com>

--- src/auth/utils/password.util.ts
+++ src/auth/utils/password.util.ts
@@ -2,6 +2,8 @@ import * as bcrypt from 'bcrypt';
 
 const BCRYPT_ROUNDS = 10;
 
+// NOTE: This is a very first level implementation. keeping it to avoid merge conflicts for now.
+// In future, consider adding peppering and more advanced techniques.
 export async function hashPassword(password: string): Promise<string> {
   return bcrypt.hash(password, BCRYPT_ROUNDS);
 }
