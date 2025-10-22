2df374f4b648c31035fab37dda9ecf3ea13a567f
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-10-22 21:18:05 +0300

    feat(constants): add rate limiting constants
    Signed-off-by: Tasneemmhammed0 <tasneemmohammed098@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-response.filter';
-import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,7 +19,6 @@ async function bootstrap() {
   );
 
   app.useGlobalFilters(new HttpExceptionFilter());
-  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 
