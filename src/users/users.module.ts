63682e1b0b3706b63d01be72310134654a68c2f5
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-10-23 00:37:36 +0300

    test(me): add update password endpoint controller unit tests
    Signed-off-by: Tasneemmhammed0 <tasneemmohammed098@gmail.com>

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
-import { MeController } from './me.controller';
+import { MeController } from './me/me.controller';
 import { BullModule } from '@nestjs/bullmq';
 @Module({
   controllers: [UsersController, MeController],
