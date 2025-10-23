a5d8a3f2ecc80923b1042d413e975daad48f25bd
mostafa <14712022100624@stud.cu.edu.eg>
2025-10-23 18:52:45 +0300

    fix: remove validations from login dto it should have been already validated in the registeration

--- src/auth/dtos/login.dto.ts
+++ src/auth/dtos/login.dto.ts
@@ -1,13 +1,9 @@
-import { IsAlphanumeric, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 
 export class LoginDto {
   @IsNotEmpty()
-  @IsString()
-  @IsAlphanumeric()
   identifier: string;
 
   @IsNotEmpty()
-  @IsString()
-  @MinLength(8)
   password: string;
 }
