1fcffeecc2807e0c7327e5e15e1cbba866cdca86
omargamal10 <ogamal1011@gmail.com>
2025-10-24 01:33:39 +0300

    fix: fix url declaration

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,13 +4,13 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  DATABASE_URL = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
+  static readonly DATABASE_URL = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
   constructor(
     config: ConfigService,
     private readonly logger: Logger,
   ) {
     super({
-      datasourceUrl: config.get('DATABASE_URL'),
+      datasourceUrl: PrismaService.DATABASE_URL,
     });
   }
 
