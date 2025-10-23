cc4e26413bddd10c41907478f5ef6e3ef8518dbc
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-10-23 13:22:49 +0300

    feat(dto): add user profile dto
    Signed-off-by: Tasneemmhammed0 <tasneemmohammed098@gmail.com>

---
+++ src/users/dtos/user-profile-response.dto.ts
@@ -0,0 +1,32 @@
+export class UserRelationshipDto {
+  blocking: boolean;
+  blockedBy: boolean;
+  following: boolean;
+  follower: boolean;
+  muted: boolean;
+}
+
+export class UserProfileDto {
+  username: string;
+  displayName: string;
+  bio: string | null;
+
+  // Should be adjusted after implementing rich text bios
+  bioEntities: null;
+
+  avatarUrl: string | null;
+  bannerUrl: string | null;
+  location: string | null;
+  websiteUrl: string | null;
+  birthDate: Date | null;
+  joinedAt: Date;
+
+  // Won't be returned for the authenticated user's own profile
+  relationship?: UserRelationshipDto;
+
+  // BigInt as string since we can't return BigInt directly in DTO
+  followingCount: string;
+  followersCount: string;
+  mutualsCount?: string | null;
+  mutualNames: string[] | null;
+}
