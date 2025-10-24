f283287da859179e13eedca71f8725ff471a0479
omargamal10 <ogamal1011@gmail.com>
2025-10-24 14:40:55 +0300

    fix: remove logger from prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,11 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   static readonly DATABASE_URL = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
-  constructor(
-    config: ConfigService,
-    private readonly logger: Logger,
-  ) {
+  private readonly logger = new Logger(PrismaService.name);
+  constructor() {
     super({
       datasourceUrl: PrismaService.DATABASE_URL,
     });
