421762cd70e52d7efe0d06d02fb4de405cf1ff8d
omargamal10 <ogamal1011@gmail.com>
2025-10-25 02:29:27 +0300

    chore: enhance the example

--- src/common/utils/create-validation-error.util.ts
+++ src/common/utils/create-validation-error.util.ts
@@ -1,9 +1,11 @@
 import { ValidationError } from 'class-validator';
 
-// for any custom error outside the class validator this should be used
-// pass proprty as the field name shown in the validation error
-// constraints as an object with key as contsraint name, and the value as the error message the client falls back to
+// use this when you need the unified validation response when it's not returned by a class-validator
+
+// pass property as the FIELD NAME SHOWN IN THE VALIDATION ERROR
+// constraints as an object with key as contsraint name, and the VALUE AS THE ERROR MESSAGE the client falls back to
 // the constraint name should then be mapped in the CONSTRAINT_TO_ERROR_CODE_MAP in http-response.filter.ts
+// YOU WILL PROBABLY FIND YOURS THERE ALREADY, those are very generic, like invalidToken for example
 
 // example usage:
 // createValidationError('recaptchaToken', { invalidToken: 'Invalid token' });
@@ -12,9 +14,25 @@ import { ValidationError } from 'class-validator';
 // and throw it in a BadRequestException
 // //so throw new BadRequestException(
 //         createValidationError('recaptchaToken', {
-//           invalidToken: 'Invalid reCAPTCHA token',
+//           invalidToken: 'The provided reCAPTCHA token is invalid or expired',
 //         }),
 //       );
+
+//this returns
+// {
+//   "success": false,
+//   "error": {
+//     "code": "VALIDATION_ERROR",
+//     "message": "Validation failed",
+//     "errors": [
+//       {
+//         "field": "recaptchaToken",
+//         "code": "INVALID_TOKEN",
+//         "message": "The provided reCAPTCHA token is invalid or expired"
+//       }
+//     ]
+//   }
+// }
 
 export function createValidationError(
   property: string,
