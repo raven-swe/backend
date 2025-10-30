88924e2cd0c83ca72a3e9dd5ecf4260ddd3b286d
mostafa <14712022100624@stud.cu.edu.eg>
2025-10-30 23:39:03 +0300

    feat: implement the notifications trigger notifications method to be called in the event listeners

---
+++ src/notifications/notifications.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { NotificationsRepository } from './notifications.repository';
+import { NotificationTriggerOptions } from './interfaces/notification-trigger.interface';
+
+@Injectable()
+export class NotificationsService {
+  constructor(private readonly notificationsRepository: NotificationsRepository) {}
+  async trigger(options: NotificationTriggerOptions) {
+    if (options.actorId === options.receiverId) return;
+
+    const existing = await this.notificationsRepository.findExisting(options);
+    if (existing) return existing;
+
+    return await this.notificationsRepository.createNotification(options);
+  }
+}
