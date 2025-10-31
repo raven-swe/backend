b588651456ea3d2521fda2c0730cec799cdfa58f
anas-ibrahem <anasibrahem101@gmail.com>
2025-10-31 21:11:25 +0200

    feat(interceptor): don't intercept redirections

--- src/common/interceptors/response.interceptor.ts
+++ src/common/interceptors/response.interceptor.ts
@@ -7,8 +7,20 @@ import { ApiSuccessResponse } from '../interfaces/response.interface';
 export class ResponseInterceptor<T> implements NestInterceptor<T, ApiSuccessResponse<T>> {
   intercept(context: ExecutionContext, next: CallHandler): Observable<ApiSuccessResponse<T>> {
     return next.handle().pipe(
-      map((response: Record<string, unknown>) => {
-        const { message, ...rest } = response;
+      map((data: any) => {
+        // Check if the response is a redirect shape
+        // If so, return it as is
+        const isRedirectShape =
+          data &&
+          typeof data === 'object' &&
+          'url' in data &&
+          typeof (data as any).url === 'string' &&
+          Object.keys(data).every((k) => k === 'url' || k === 'statusCode');
+        if (isRedirectShape) {
+          return data;
+        }
+
+        const { message, ...rest } = data as Record<string, unknown>;
         return {
           success: true,
           message: message as string | undefined,
