7ffb9d246d57b4ddf4af846678bceda224787696
mostafa <14712022100624@stud.cu.edu.eg>
2025-10-31 19:16:40 +0200

    feat: make the jwt throw early if user in token doesn't match a db user and if match put the db user in request

--- src/auth/jwt.strategy.spec.ts
+++ src/auth/jwt.strategy.spec.ts
@@ -2,20 +2,30 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { JwtStrategy } from './jwt.strategy';
 import { ConfigService } from '@nestjs/config';
 import { RequestUser } from './types';
-
-const mockConfigService = {
-  get: jest.fn((key: string) => {
-    if (key === 'JWT_SECRET') return 'test-secret';
-    return null;
-  }),
-};
+import { UsersService } from 'src/users/users.service';
+import { JwtPayload } from './types/jwt-payload.type';
+import { HttpException } from '@nestjs/common';
 
 describe('JwtStrategy', () => {
   let strategy: JwtStrategy;
 
+  const mockConfigService = {
+    get: jest.fn((key: string) => {
+      if (key === 'JWT_SECRET') return 'test-secret';
+      return null;
+    }),
+  };
+  const mockUsersService: jest.Mocked<Partial<UsersService>> = {
+    checkIfUserExistsAndActive: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [JwtStrategy, { provide: ConfigService, useValue: mockConfigService }],
+      providers: [
+        JwtStrategy,
+        { provide: ConfigService, useValue: mockConfigService },
+        { provide: UsersService, useValue: mockUsersService },
+      ],
     }).compile();
 
     strategy = module.get<JwtStrategy>(JwtStrategy);
@@ -27,11 +37,24 @@ describe('JwtStrategy', () => {
 
   describe('validate', () => {
     it('should return the user payload from the JWT', async () => {
-      const payload: RequestUser = { id: '1' };
+      const payload: JwtPayload = { id: '1' };
+      const user: Partial<RequestUser> = { id: BigInt(payload.id) };
+
+      (mockUsersService.checkIfUserExistsAndActive as jest.Mock).mockResolvedValueOnce(
+        user as RequestUser,
+      );
 
       const result = await strategy.validate(payload);
 
-      expect(result).toEqual(payload);
+      expect(result).toEqual(user);
+    });
+
+    it('should return the user payload from the JWT', async () => {
+      const payload: JwtPayload = { id: '1' };
+
+      (mockUsersService.checkIfUserExistsAndActive as jest.Mock).mockResolvedValueOnce(null);
+
+      await expect(strategy.validate(payload)).rejects.toThrow(HttpException);
     });
   });
 });
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,12 +1,18 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { RequestUser } from './types';
+import { JwtPayload } from './types/jwt-payload.type';
+import { UsersService } from 'src/users/users.service';
+import { USERS_ERROR_CODES, USERS_ERROR_MESSAGES } from 'src/common/constants/users.constants';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly config: ConfigService) {
+  constructor(
+    private readonly config: ConfigService,
+    private readonly usersService: UsersService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -14,7 +20,17 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: RequestUser) {
-    return Promise.resolve(payload);
+  async validate(payload: JwtPayload): Promise<RequestUser> {
+    const user = await this.usersService.checkIfUserExistsAndActive(BigInt(payload.id));
+    if (!user) {
+      throw new HttpException(
+        {
+          message: USERS_ERROR_MESSAGES.USER_NOT_FOUND,
+          code: USERS_ERROR_CODES.USER_NOT_FOUND,
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+    return user;
   }
 }
