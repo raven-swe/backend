8a9ea7795d22dd2fd8f2f1950791d5fb7dd23c45
mostafa <14712022100624@stud.cu.edu.eg>
2025-10-31 19:17:05 +0200

    feat: add field selection from the user decorator

--- src/auth/decorators/user.decorator.ts
+++ src/auth/decorators/user.decorator.ts
@@ -1,9 +1,17 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
+import { RequestUser } from '../types';
 
-export const getUserFromContext = (data: unknown, ctx: ExecutionContext) => {
+export const getUserFromContext = (
+  data: keyof RequestUser | undefined,
+  ctx: ExecutionContext,
+): RequestUser[keyof RequestUser] | RequestUser | undefined => {
   const request: Request = ctx.switchToHttp().getRequest();
-  return request.user;
+  const user = request.user as RequestUser | undefined;
+  if (!user) {
+    return undefined;
+  }
+  return data ? user[data] : user;
 };
 
 export const User = createParamDecorator(getUserFromContext);
