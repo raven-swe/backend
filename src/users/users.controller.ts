abf4e6838db83aac1f070a6532265b719d00407d
Tasneem Mohammed <149891742+Tasneemmohammed0@users.noreply.github.com>
2025-11-01 00:53:56 +0200

    feat: implement user interactions endpoints (follow, unfollow, block, unblock, mute, unmute) - [CU-869at0u3k] (#58)
    Signed-off-by: Tasneemmhammed0 <tasneemmohammed098@gmail.com>

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Param, Post, UseGuards, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { User } from 'src/auth/decorators';
-import type { RequestUser } from 'src/auth/types/user.type';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { User } from 'src/auth/decorators';
+import type { RequestUser } from 'src/auth/types';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post(':username/following')
+  @UseGuards(JwtAuthGuard)
+  async followUser(@Param('username') username: string, @User() user: RequestUser) {
+    await this.usersService.followUser(BigInt(user.id), username);
+    return { message: 'Followed user successfully' };
+  }
+
+  @Delete(':username/following')
+  @UseGuards(JwtAuthGuard)
+  async unfollowUser(@Param('username') username: string, @User() user: RequestUser) {
+    await this.usersService.unfollowUser(BigInt(user.id), username);
+    return { message: 'Unfollowed user successfully' };
+  }
 
   @Get(':username/profile')
   // TODO: This should be optional guard (if logged in, provide more details (just the relations))
