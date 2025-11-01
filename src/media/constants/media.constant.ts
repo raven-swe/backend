131c5698373ed7a4e23ba3b8cb882f5c8daa173c
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-11-01 15:53:57 +0200

    feat: add delete file function
    Signed-off-by: Tasneemmhammed0 <tasneemmohammed098@gmail.com>

--- src/media/constants/media.constant.ts
+++ src/media/constants/media.constant.ts
@@ -6,9 +6,15 @@ export const ALLOWED_EXTENSIONS = [...IMAGE_EXTENSIONS, ...VIDEO_EXTENSIONS];
 export const MEDIA_CODES = {
   MEDIA_UPLOAD_SAVE_FAILED: 'MEDIA_UPLOAD_SAVE_FAILED',
   NO_FILES_PROVIDED: 'NO_FILES_PROVIDED',
+  MEDIA_NOT_FOUND: 'MEDIA_NOT_FOUND',
+  INVALID_URL: 'INVALID_URL',
+  UNAUTHORIZED_DELETE: 'UNAUTHORIZED_DELETE',
 };
 
 export const MEDIA_MESSAGES = {
   MEDIA_UPLOAD_SAVE_FAILED: 'Failed to upload and save media',
   NO_FILES_PROVIDED: 'No files provided for upload',
+  MEDIA_NOT_FOUND: 'Media not found',
+  INVALID_URL: 'Invalid URL',
+  UNAUTHORIZED_DELETE: 'Unauthorized delete attempt',
 };
