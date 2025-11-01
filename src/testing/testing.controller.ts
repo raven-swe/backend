0c0548dba5d3315b94c0d1367fd6b3e3a2355652
omargamal10 <ogamal1011@gmail.com>
2025-11-01 23:50:55 +0200

    fix: make it a get with query params

--- src/testing/testing.controller.ts
+++ src/testing/testing.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { TestService } from './testing.service';
 import { GetOtpDto } from './dtos/get-otp.dto';
 
@@ -7,7 +7,7 @@ export class TestController {
   constructor(private readonly testService: TestService) {}
 
   @Get('otp')
-  async getOtpForTesting(@Body() getOtpDto: GetOtpDto) {
+  async getOtpForTesting(@Query() getOtpDto: GetOtpDto) {
     return this.testService.getOtp(getOtpDto);
   }
 }
