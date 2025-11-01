cf4e62b0f42b33548e29f273edc479702526ea0d
omargamal10 <ogamal1011@gmail.com>
2025-11-02 01:27:14 +0200

    fix: add controller to tweets module and add auth guard

--- src/tweets/timeline/timeline.controller.ts
+++ src/tweets/timeline/timeline.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, HttpCode, Query } from '@nestjs/common';
+import { Controller, Get, HttpCode, Query, UseGuards } from '@nestjs/common';
 import { User } from 'src/auth/decorators';
 import { PaginationQueryDto } from 'src/common/pagination-query.dto';
 import { TweetsService } from '../tweets.service';
 import type { RequestUser } from 'src/auth/types';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('timeline')
 export class TimelineController {
   constructor(private readonly tweetsService: TweetsService) {}
--- src/tweets/tweets.module.ts
+++ src/tweets/tweets.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TweetsService } from './tweets.service';
 import { TweetsController } from './tweets.controller';
 import { TweetsRepository } from './tweets.repository';
+import { TimelineController } from './timeline/timeline.controller';
 
 @Module({
   providers: [TweetsService, TweetsRepository],
-  controllers: [TweetsController],
+  controllers: [TweetsController, TimelineController],
 })
 export class TweetsModule {}
