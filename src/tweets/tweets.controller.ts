0eb22736cff72fa12da544800996d8bad728ef22
Omar Gamal <ogamal1011@gmail.com>
2025-11-02 18:31:02 +0200

    feat: basic timeline seed and implementation - [CU-869aykjw6] (#75)

--- src/tweets/tweets.controller.ts
+++ src/tweets/tweets.controller.ts
@@ -1,6 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
+import { TweetsService } from './tweets.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('tweets')
+@UseGuards(JwtAuthGuard)
 export class TweetsController {
+  constructor(private readonly tweetsService: TweetsService) {}
+
   // --------------------------------------
 }
