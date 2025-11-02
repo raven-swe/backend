9f02b93535b1e9a540b4348a869a4fc6be6e3d00
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-11-02 03:25:06 +0200

    fix: remove prisma service form tweet providers
    Signed-off-by: Tasneemmhammed0 <tasneemmohammed098@gmail.com>

--- src/tweets/tweets.module.ts
+++ src/tweets/tweets.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TweetsService } from './tweets.service';
 import { TweetsController } from './tweets.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { TweetsRepository } from './tweets.repository';
 import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  providers: [TweetsService, PrismaService, TweetsRepository],
+  providers: [TweetsService, TweetsRepository],
   controllers: [TweetsController],
   exports: [TweetsService],
   imports: [UsersModule],
