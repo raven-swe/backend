e4d05ae842fd5020f5c7086aef11b54e62a5c4db
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-11-18 13:52:16 +0200

    feat: edit tweet dto
    Signed-off-by: Tasneemmhammed0 <tasneemmohammed098@gmail.com>

--- src/tweets/dtos/tweet.dto.ts
+++ src/tweets/dtos/tweet.dto.ts
@@ -1,11 +1,12 @@
 import { TweetEntitiesDto } from './tweet-entitites.dto';
 import { AuthorDto } from './author.dto';
+import { MediaDto } from 'src/media/dtos';
 
 export class TweetDto {
   id: string;
   author: AuthorDto;
   //TODO null content will be handled later for media tweets
-  content: string;
+  content: string | null;
   createdAt: Date;
 
   replyCount: number;
@@ -16,7 +17,7 @@ export class TweetDto {
   isRetweeted: boolean;
 
   entities: TweetEntitiesDto;
-  media: [];
+  media: MediaDto[];
   // TODO media field will be added later
 
   replyToTweetId: string | null;
