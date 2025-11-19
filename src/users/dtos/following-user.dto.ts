d370401fc4d6956ab4950dabd15cff3a37ee628d
mostafa <14712022100624@stud.cu.edu.eg>
2025-11-19 16:17:32 +0200

    feat: add followsYou flag to the following user dto

--- src/users/dtos/following-user.dto.ts
+++ src/users/dtos/following-user.dto.ts
@@ -2,5 +2,6 @@ import { CompactUserDto } from './compact-user.dto';
 
 export class FollowingUserDto extends CompactUserDto {
   isFollowing: boolean;
+  followsYou: boolean;
   isBlocked: boolean;
 }
