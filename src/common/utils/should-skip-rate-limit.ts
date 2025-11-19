607211f5ffedcb72fec7b28c5eaa528ef97ca6fd
Amr Samy <samym5468@gmail.com>
2025-11-19 11:52:54 +0200

    feat: skip rate limiting in testing environment

--- src/common/utils/should-skip-rate-limit.ts
+++ src/common/utils/should-skip-rate-limit.ts
@@ -5,11 +5,6 @@ import { Request } from 'express';
   Skip rate limiting in testing environment
 */
 export function shouldSkipRateLimit(context: ExecutionContext): boolean {
-  if (process.env.NODE_ENV !== 'testing') return false;
-
-  const request = context.switchToHttp().getRequest<Request>();
-  const secret = process.env.RATE_LIMIT_BYPASS_SECRET;
-
-  const header = request.headers['x-bypass-rate-limit'];
-  return typeof header === 'string' && header === secret;
+  if (process.env.NODE_ENV === 'testing') return true;
+  return false;
 }
