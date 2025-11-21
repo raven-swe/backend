c0ceface8bff21c973b722ba3b1403f1916e767f
omargamal10 <ogamal1011@gmail.com>
2025-11-22 00:03:23 +0200

    chore: fix

--- src/trending/trending.service.spec.ts
+++ src/trending/trending.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TrendingService } from './trending.service';
+import { TrendingRepository } from './trending.repository';
 
 describe('TrendingService', () => {
   let service: TrendingService;
 
+  const mockTrendingRepository = {
+    getOrCreateHashtagIds: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TrendingService],
+      providers: [
+        TrendingService,
+        {
+          provide: TrendingRepository,
+          useValue: mockTrendingRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<TrendingService>(TrendingService);
