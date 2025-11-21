2d42784a0df5b7f3c45ad72788e00279f662f0c1
Amr Samy <41027803+AmrSamy59@users.noreply.github.com>
2025-11-21 14:23:37 +0200

    feat: Testing Environment Setup - [CU-869b7z63c] (#104)
    Co-authored-by: mostafa <14712022100624@stud.cu.edu.eg>
    Co-authored-by: OmarHassan2003 <omarhassanelsherif2003@gmail.com>
    Co-authored-by: Saif <78622218+im-saif@users.noreply.github.com>
    Co-authored-by: Saif <saifeldin402@gmail.com>

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1065,4 +1065,13 @@ export class UsersRepository {
       return { bannerUrl: profile?.bannerUrl || null };
     });
   }
+
+  async createProfile(userId: bigint, displayName: string) {
+    return await this.prisma.profile.create({
+      data: {
+        userId,
+        displayName,
+      },
+    });
+  }
 }
