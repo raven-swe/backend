85160c5c45111308ac7f014099515687bded1c4e
omargamal10 <ogamal1011@gmail.com>
2025-11-23 01:56:34 +0200

    feat: optimize creating and getting hashtags for one database round trip and add incrementing hashtag count

--- src/trending/trending.repository.ts
+++ src/trending/trending.repository.ts
@@ -15,7 +15,7 @@ export class TrendingRepository {
    */
   async getOrCreateHashtagIds(
     hashtags: PlainHashtag[],
-    prismaClient: Prisma.TransactionClient,
+    prismaClient: Prisma.TransactionClient = this.prisma,
   ): Promise<bigint[]> {
     if (!hashtags || hashtags.length === 0) {
       return [];
@@ -24,38 +24,37 @@ export class TrendingRepository {
     // for less db work, they are case-insensitive anyways
     const keywords = Array.from(new Set(hashtags.map((hashtag) => hashtag.keyword.toLowerCase())));
 
-    const existing = await prismaClient.trendingKeyword.findMany({
-      where: { keyword: { in: keywords }, isHashtag: true },
-      select: { id: true, keyword: true },
-    });
+    const results = await prismaClient.$queryRaw<{ id: bigint; keyword: string }[]>`
+      INSERT INTO "trending_keywords" (keyword, "is_hashtag", count)
+      VALUES ${Prisma.join(keywords.map((keyword) => Prisma.sql`(${keyword}, true, 1)`))}
+      ON CONFLICT (keyword, "is_hashtag")
+      DO UPDATE SET count = "trending_keywords".count + 1
+      RETURNING id, keyword
+    `;
 
     const map = new Map<string, bigint>(
-      existing.map((item) => [item.keyword.toLowerCase(), item.id]),
+      results.map((item) => [item.keyword.toLowerCase(), item.id]),
     );
-    const missing = keywords.filter((keyword) => !map.has(keyword));
-
-    if (missing.length > 0) {
-      await prismaClient.trendingKeyword.createMany({
-        data: missing.map((keyword) => ({
-          keyword,
-          isHashtag: true,
-          count: 1,
-        })),
-        skipDuplicates: true,
-      });
-
-      // there is no other way to do this in a transaction without a refetch
-      const newlyAddedTags = await prismaClient.trendingKeyword.findMany({
-        where: { keyword: { in: missing }, isHashtag: true },
-        select: { id: true, keyword: true },
-      });
-
-      newlyAddedTags.forEach((item) => map.set(item.keyword.toLowerCase(), item.id));
-    }
 
     // map ids to original input
     // the ! at the end is safe, we are sure all exist
     return hashtags.map((hashtag) => map.get(hashtag.keyword.toLowerCase())!);
+  }
+
+  async incrementHashtagCount(
+    hashtagIds: bigint[],
+    prismaClient: Prisma.TransactionClient = this.prisma,
+  ) {
+    await prismaClient.trendingKeyword.updateMany({
+      where: {
+        id: { in: hashtagIds },
+      },
+      data: {
+        count: {
+          increment: 1,
+        },
+      },
+    });
   }
 
   // ---------------
