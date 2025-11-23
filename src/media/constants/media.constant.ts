74cf29da2a290180a6aef64cc460088c60ec4ee9
Tasneem Mohammed <149891742+Tasneemmohammed0@users.noreply.github.com>
2025-11-23 18:02:32 +0200

    feat: add webp ext to image extensions - [CU-869b1765a] (#88)
    Signed-off-by: Tasneemmhammed0 <tasneemmohammed098@gmail.com>
    Co-authored-by: Omar Gamal <ogamal1011@gmail.com>
    Co-authored-by: Mostafa Ahmed Abdelglel <139139781+galelo04@users.noreply.github.com>

--- src/media/constants/media.constant.ts
+++ src/media/constants/media.constant.ts
@@ -1,6 +1,6 @@
 export const MAX_FILE_SIZE_BYTES = 5 * 1024 * 1024; // 5MB
+export const IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png', 'webp'];
 export const MAX_VIDEO_FILE_SIZE_BYTES = 10 * 1024 * 1024; // 10MB
-export const IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png'];
 export const VIDEO_EXTENSIONS = ['mp4', 'mkv', 'webm', 'mov'];
 export const GIF_EXTENSIONS = ['gif'];
 export const ALLOWED_EXTENSIONS = [...IMAGE_EXTENSIONS, ...VIDEO_EXTENSIONS, ...GIF_EXTENSIONS];
