6f4dd6694d88072557671f0b0ab213f4ddd9a051
OmarHassan2003 <omarhassanelsherif2003@gmail.com>
2025-12-03 22:54:29 +0200

    chore: remove unneeded logs

--- src/sse/event-publisher.service.ts
+++ src/sse/event-publisher.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { SseService } from './sse.service';
 
 export interface SseEvent {
@@ -8,17 +8,13 @@ export interface SseEvent {
 
 @Injectable()
 export class EventPublisherService {
-  private readonly logger = new Logger(EventPublisherService.name);
-
   constructor(private readonly sse: SseService) {}
 
   async publishToUser(userId: string, event: SseEvent) {
-    this.logger.log(`Publishing ${event.event} to user ${userId}`);
     await this.sse.publish(userId, event);
   }
 
   async publishToUsers(userIds: string[], event: SseEvent) {
-    this.logger.log(`Publishing ${event.event} to ${userIds.length} users`);
     await Promise.all(userIds.map((userId) => this.sse.publish(userId, event)));
   }
 }
