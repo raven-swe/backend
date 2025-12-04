7fd5f1aa5690d380ebc6f014eece8096ea57c685
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-12-04 09:21:02 +0200

    fix: add missing import
    Signed-off-by: Tasneemmhammed0 <tasneemmohammed098@gmail.com>

--- test/users/users.service.spec.ts
+++ test/users/users.service.spec.ts
@@ -13,6 +13,7 @@ import { USERS_ERROR_CODES, USERS_ERROR_MESSAGES } from 'src/users/constants';
 import { MediaService } from 'src/media/media.service';
 import { MediaFolder } from 'src/media/enums';
 import { ContentParsingService } from 'src/content-parsing/content-parsing.service';
+import { NewUser } from 'src/users/interfaces';
 
 jest.mock('src/auth/utils/password.util');
 jest.mock('src/users/utils/validate-password-format.util');
