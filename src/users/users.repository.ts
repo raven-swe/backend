98134765aded4b6c3d13333f3b473e3665aff651
LoayAhmed304 <loayahmed304@gmail.com>
2025-12-07 20:27:14 +0200

    feat: disable allowing notifications on a blocked user

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1331,6 +1331,19 @@ export class UsersRepository {
   }
 
   async toggleUserNotifications(userId: bigint, followedId: bigint, enable: boolean) {
+    if (enable) {
+      const isBlocked = await this.areUsersBlocked(userId, followedId);
+      if (isBlocked) {
+        throw new HttpException(
+          {
+            message: USERS_ERROR_MESSAGES.CANNOT_FOLLOW_USER,
+            code: USERS_ERROR_CODES.CANNOT_FOLLOW_USER,
+          },
+          HttpStatus.FORBIDDEN,
+        );
+      }
+    }
+
     return await this.prisma.follow.upsert({
       where: {
         followerId_followedId: { followerId: userId, followedId: followedId },
