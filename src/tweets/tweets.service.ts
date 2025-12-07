fa147a0e1b2e5ec9f177bacb0164883465cc83bf
Ahmed Sobhy <48056730+AhmedSobhy01@users.noreply.github.com>
2025-12-07 15:05:21 +0200

    fix: check for empty tweets

--- src/tweets/tweets.service.ts
+++ src/tweets/tweets.service.ts
@@ -49,8 +49,9 @@ export class TweetsService {
         HttpStatus.BAD_REQUEST,
       );
     }
+    const trimContent = createTweetDto.content?.trim() ?? '';
 
-    if (!createTweetDto.content && (!createTweetDto.media || createTweetDto.media.length === 0)) {
+    if (!trimContent && (!createTweetDto.media || createTweetDto.media.length === 0)) {
       throw new HttpException(
         {
           message: TWEETS_ERROR_MESSAGES.INVALID_TWEET_PAYLOAD,
@@ -59,6 +60,9 @@ export class TweetsService {
         HttpStatus.BAD_REQUEST,
       );
     }
+
+    createTweetDto.content = trimContent;
+
     if (createTweetDto.media && createTweetDto.media.length > 4) {
       throw new HttpException(
         {
