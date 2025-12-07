74cca4781481fd66ce6d0a97846dcaab4ff27071
OmarHassan2003 <omarhassanelsherif2003@gmail.com>
2025-12-07 19:53:03 +0200

    fix: add actual migration to make relation between sessions and refresh tokens 1-1

---
+++ prisma/migrations/20251207171146_make_refreshtoken_session_one_to_one/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `search_document` on the `tweets` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[session_id]` on the table `refresh_tokens` will be added. If there are existing duplicate values, this will fail.
+  - Made the column `session_id` on table `refresh_tokens` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."refresh_tokens" DROP CONSTRAINT "refresh_tokens_session_id_fkey";
+
+-- DropIndex
+DROP INDEX "public"."tweets_search_document_idx";
+
+-- AlterTable
+ALTER TABLE "refresh_tokens" ALTER COLUMN "session_id" SET NOT NULL;
+
+-- AlterTable
+ALTER TABLE "tweets" DROP COLUMN "search_document";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "refresh_tokens_session_id_key" ON "refresh_tokens"("session_id");
+
+-- AddForeignKey
+ALTER TABLE "refresh_tokens" ADD CONSTRAINT "refresh_tokens_session_id_fkey" FOREIGN KEY ("session_id") REFERENCES "sessions"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
