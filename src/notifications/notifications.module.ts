9c89fe8d336f15ac95d0b1193a125cc5f14be130
mostafa <14712022100624@stud.cu.edu.eg>
2025-12-09 23:31:02 +0200

    fix: import the sse module as forwardRef in the notifications module

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsRepository } from './notifications.repository';
@@ -13,7 +13,7 @@ import { UsersModule } from 'src/users/users.module';
 @Module({
   imports: [
     TweetsModule,
-    SseModule,
+    forwardRef(() => SseModule),
     UsersModule,
     DevicesModule,
     BullModule.registerQueue({ name: 'notifications' }),
