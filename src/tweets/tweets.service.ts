98bced468d889e561089a4ea2ba5b0fdf9abf083
mostafa <14712022100624@stud.cu.edu.eg>
2025-12-11 02:04:43 +0200

    refactor: add the optional guard to the get tweet and adjut service and repo accordingly

--- src/tweets/tweets.service.ts
+++ src/tweets/tweets.service.ts
@@ -665,7 +665,7 @@ export class TweetsService {
     return { items, pagination };
   }
 
-  async getTweet(tweetId: bigint, currentUserId: bigint): Promise<ThreadViewResponseDto> {
+  async getTweet(tweetId: bigint, currentUserId: bigint | null): Promise<ThreadViewResponseDto> {
     const tweet = await this.tweetsRepository.getDetailedTweetById(tweetId, currentUserId);
 
     if (!tweet) {
@@ -676,6 +676,10 @@ export class TweetsService {
         },
         HttpStatus.NOT_FOUND,
       );
+    }
+
+    if (!currentUserId) {
+      return { ...tweet, rootTweet: null, parentTweets: [], hasMoreParents: false };
     }
 
     let rootTweet: TweetDto | DeletedTweet | null = null;
