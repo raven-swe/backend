615ce64da8c3936a53bbf6261639c3c18cbf6fa8
Mostafa Ahmed Abdelglel <139139781+galelo04@users.noreply.github.com>
2025-12-13 20:37:13 +0200

    feat: guest users - [CU-869bev5er] (#192)

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+import { OPTIONAL_AUTH_KEY } from 'src/common/decorators/optional-auth.decorator';
+import { RequestUser } from 'src/common/interfaces';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context);
+  }
+
+  handleRequest(err: any, user: any, _info: any, context: ExecutionContext): any {
+    const isOptional = this.reflector.getAllAndOverride<boolean>(OPTIONAL_AUTH_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (user) {
+      return user as RequestUser;
+    }
+
+    if (isOptional) {
+      return null;
+    }
+
+    throw (err as Error) || new UnauthorizedException();
+  }
+}
