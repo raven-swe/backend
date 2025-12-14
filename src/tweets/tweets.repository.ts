9c80356ded80fff65c4d7f6ab7967cd4b23d4120
omargamal10 <ogamal1011@gmail.com>
2025-12-15 00:46:33 +0200

    chore: merge conflicts

--- src/tweets/tweets.repository.ts
+++ src/tweets/tweets.repository.ts
@@ -977,6 +977,7 @@ export class TweetsRepository {
   }
 
   async hydrateTweetsInList(authUserId: bigint, tweetIds: bigint[]) {
+    console.log(authUserId);
     return await this.prisma.tweet.findMany({
       where: {
         id: { in: tweetIds },
